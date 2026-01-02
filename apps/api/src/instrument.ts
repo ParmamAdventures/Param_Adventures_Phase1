1318362f41412811712b2464079f3648d863c3ac
Akash Bhat <akashkbhat216@gmail.com>
2026-01-03 00:56:04 +0530

    refactor(api): move sentry init to instrument.ts

---
+++ apps/api/src/instrument.ts
@@ -0,0 +1,17 @@
+import * as Sentry from "@sentry/node";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
+
+// Ensure Sentry is initialized as early as possible
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    nodeProfilingIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+  // Profiling
+  profilesSampleRate: 1.0,
+});
+
+console.log("Param Adventures API: Sentry Instrumented 🔭");
