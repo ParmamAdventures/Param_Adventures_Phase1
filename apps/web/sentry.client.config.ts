0b65c6007f03cc920cdf94e1a172b7be1ae1b278
Akash Bhat <akashkbhat216@gmail.com>
2026-01-02 23:24:25 +0530

    chore: enable sentry debug logging

--- apps/web/sentry.client.config.ts
+++ apps/web/sentry.client.config.ts
@@ -7,5 +7,7 @@ Sentry.init({
   tracesSampleRate: 1.0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  debug: true,
 });
+
+console.log("Param Adventures: Sentry Client SDK Initialized 🚀");
