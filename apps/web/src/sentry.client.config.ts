507494d0a8e8c2a92cd2ebd64bf8b0a689df28ee
Akash Bhat <akashkbhat216@gmail.com>
2026-01-03 10:12:04 +0530

    chore(sentry): disable debug logs for production

--- apps/web/src/sentry.client.config.ts
+++ apps/web/src/sentry.client.config.ts
@@ -7,7 +7,7 @@ Sentry.init({
   tracesSampleRate: 1.0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: true,
+  debug: false,
 });
 
 console.log("Param Adventures: Sentry Client SDK Initialized 🚀");
