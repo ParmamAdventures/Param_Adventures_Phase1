e0bd42104079edc6a21c377ce876575ebe5fda46
Akash Bhat <akashkbhat216@gmail.com>
2026-01-03 14:47:14 +0530

    fix(api): further augment Express Request and relax User type props

--- apps/api/src/types/express.d.ts
+++ apps/api/src/types/express.d.ts
@@ -1,14 +1,16 @@
 
-import { Role } from "@prisma/client";
-
 declare global {
   namespace Express {
     interface User {
       id: string;
-      email: string;
-      name: string;
-      roles: string[]; // Simplification given CI error on Role enum sometimes
+      email?: string;
+      name?: string;
+      roles: string[];
       permissions?: string[];
+    }
+
+    interface Request {
+      permissions?: string[]; // Augmening Request to include permissions
     }
   }
 }
